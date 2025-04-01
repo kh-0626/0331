27652bc8adc7acc1cb43bfe9a6b16bc9bcd2ef34
kh-0626 <kdw052516@naver.com>
2025-04-01 17:20:36 +0900

    20250401

---
+++ start/src/main/java/com/kh/start/member/model/vo/Member.java
@@ -0,0 +1,20 @@
+package com.kh.start.member.model.vo;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Value;
+
+// Value Object
+// V	 O
+
+@Value
+@Getter
+@Builder
+public class Member {
+	
+	private Long memberNo;
+	private String memberId;
+	private String memberPw;
+	private String memberName;
+	private String role;
+}
